20460b84b14d6fb0d71dd59d5415ed7144d0005a
Shivanand <kolishivanand4@gmail.com>
2026-06-05 22:16:17 +0530

    Initial Commit

---
+++ OliveUPI/OliveUPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OliveUPIApp.swift
+//  OliveUPI
+//
+//  Created by Shivanand Koli on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct OliveUPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OliveUPITests/OliveUPITests.swift
@@ -0,0 +1,19 @@
+//
+//  OliveUPITests.swift
+//  OliveUPITests
+//
+//  Created by Shivanand Koli on 05/06/26.
+//
+
+import Testing
+@testable import OliveUPI
+
+struct OliveUPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
